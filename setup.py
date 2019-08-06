1c81f7f06c9def906df0bb22d36206448e45a069
Nathan Buckner <nbuckner@roku.com>
2019-08-06 12:31:34 -0700

    Initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+VERSION = "0.18"
+
+setup(
+    name="versioneer-518",
+    license="public domain",
+    version=VERSION,
+    description="Just the vendored file",
+    author="Nathan Buckner",
+    author_email="bucknerns@gmail.com",
+    url="https://github.com/warner/python-versioneer",
+    py_modules=["versioneer"])
